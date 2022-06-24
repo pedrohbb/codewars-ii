4250eeb2a72b9b980ffe40f698f075d418de3932
Thainara Furforo <thaifurforo@gmail.com>
2022-06-24 19:15:31 -0300

    reorganize db files

--- script-modelo-dados.sql
+++ script-modelo-dados.sql
@@ -5,32 +5,36 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema code_wars_II
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema code_wars_ii
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema code_wars_II
+-- Schema code_wars_ii
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `code_wars_II` DEFAULT CHARACTER SET utf8 ;
-USE `code_wars_II` ;
+CREATE SCHEMA IF NOT EXISTS `code_wars_ii` DEFAULT CHARACTER SET utf8mb3 ;
+USE `code_wars_ii` ;
 
 -- -----------------------------------------------------
--- Table `code_wars_II`.`Cargos`
+-- Table `code_wars_ii`.`cargos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `code_wars_II`.`Cargos` (
+CREATE TABLE IF NOT EXISTS `code_wars_ii`.`cargos` (
   `codigo_cargo` INT NOT NULL,
   `descricao` VARCHAR(40) NOT NULL,
   `salario_base` FLOAT NOT NULL,
   `comissao` FLOAT NOT NULL,
   PRIMARY KEY (`codigo_cargo`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `code_wars_II`.`Funcionarios`
+-- Table `code_wars_ii`.`funcionarios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `code_wars_II`.`Funcionarios` (
-  `matricula` INT(6) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `code_wars_ii`.`funcionarios` (
+  `matricula` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(100) NOT NULL,
   `cpf` CHAR(11) NOT NULL,
   `data_admissao` DATE NOT NULL,
@@ -41,26 +45,26 @@ CREATE TABLE IF NOT EXISTS `code_wars_II`.`Funcionarios` (
   INDEX `fk_Funcionarios_Cargos_idx` (`codigo_cargo` ASC) VISIBLE,
   CONSTRAINT `fk_Funcionarios_Cargos`
     FOREIGN KEY (`codigo_cargo`)
-    REFERENCES `code_wars_II`.`Cargos` (`codigo_cargo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `code_wars_ii`.`cargos` (`codigo_cargo`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `code_wars_II`.`holerite`
+-- Table `code_wars_ii`.`holerite`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `code_wars_II`.`holerite` (
+CREATE TABLE IF NOT EXISTS `code_wars_ii`.`holerite` (
   `mes/ano` CHAR(7) NOT NULL,
-  `matricula` INT(6) NOT NULL,
+  `matricula` INT NOT NULL,
   `faltas` FLOAT NOT NULL,
+  `inss` FLOAT NOT NULL,
+  `irrf` FLOAT NOT NULL,
   INDEX `fk_holerite_Funcionarios1_idx` (`matricula` ASC) VISIBLE,
   CONSTRAINT `fk_holerite_Funcionarios1`
     FOREIGN KEY (`matricula`)
-    REFERENCES `code_wars_II`.`Funcionarios` (`matricula`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `code_wars_ii`.`funcionarios` (`matricula`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
